3415aaced3d44130722066ae3680014b24776ef1
Donna <gretayeungg@gmail.com>
2023-07-04 22:05:04 +0800

    04_removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
-const reverseString = function(string) {
+const reverseString = function (string) {
     let word = "";
-    for(x = 1; x <= string.length; x++) {
-        word += string.substr(-x,1);
+    for (x = 1; x <= string.length; x++) {
+        word += string.substr(-x, 1);
     }
     return word;
 };
