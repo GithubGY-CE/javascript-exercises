bade04a1c00022264c083e87d04fad7282e2b1f8
Donna <gretayeungg@gmail.com>
2023-07-04 23:23:08 +0800

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (numberFrom, numberTo) {
 
+    if (Math.sign(numberTo) === -1 || 
+        Math.sign(numberFrom) === -1 || 
+        isNaN(numberFrom) || 
+        isNaN(numberTo) || 
+        typeof numberFrom === "string" || 
+        typeof numberTo === 'string') {
+            
+        return "ERROR";
+    } else if (numberFrom < numberTo) {
+        let sum = 0;
+        for (x = numberFrom; x <= numberTo; x++) {
+            sum += x;
+        }
+        return sum;
+    } else {
+        let sum = 0;
+            for (x = numberTo; x <= numberFrom; x++) {
+                sum += x;
+            }
+            return sum;
+    }
 };
 
 // Do not edit below this line
