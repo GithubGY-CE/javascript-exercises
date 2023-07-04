708f0b8505fe0393c9cfd75b97be56e147ae8004
Donna <gretayeungg@gmail.com>
2023-07-04 18:52:08 +0800

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let word = "";
+    for(x = 1; x <= string.length; x++) {
+        word += string.substr(-x,1);
+    }
+    return word;
 };
 
 // Do not edit below this line
