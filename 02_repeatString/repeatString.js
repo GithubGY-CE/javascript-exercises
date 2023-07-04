e6c56496eeffa9e14bc215cb3e3db906f1fe2400
Donna <gretayeungg@gmail.com>
2023-07-04 17:33:19 +0800

    02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    if (!number) {
+        return "";
+    } else if (Math.sign(number) === -1) {
+        return "ERROR";
+    }
+    else {
+        let newString = string;
+        for (x = 1; x < number; x++) {
+            newString += string;
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
