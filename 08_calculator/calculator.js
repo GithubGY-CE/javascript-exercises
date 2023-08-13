1dc1a83d0c77de02372a30978b1d905bf0996ac0
Donna <gretayeungg@gmail.com>
2023-08-13 17:42:10 +0800

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+  return numberArray.reduce((sum,number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...numberArray) {
+  return numberArray.reduce((product,number) => product * number);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for(let x = num ; x > 1; x--) {
+    factorial *= x;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
