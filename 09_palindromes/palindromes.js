93c7bdb5bb1856e0d7f3d69b24060d6fa4b1580b
Donna <gretayeungg@gmail.com>
2023-08-14 10:41:56 +0800

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    words = words.replace(/\W/g, "").toLowerCase();
+    const reverseWords = words.split("").reverse().join("");
+    return reverseWords == words;
 };
+
+//npm run test palindromes.spec.js
 
 // Do not edit below this line
 module.exports = palindromes;
