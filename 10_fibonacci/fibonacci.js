aa30c8235fc6ee7cb62f341510353a9665efd9a4
Donna <gretayeungg@gmail.com>
2023-08-14 11:51:31 +0800

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    else if (num === 1 || num === 2) {
+        return 1;
+    } else {
+        let firstNum = 1;
+        let secondNum = 1;
+        let container;
+        let x = 2;
+       for (x = 2; x < num; x++) {
+            container = secondNum; 
+            secondNum += firstNum;
+            firstNum = container;
+        }
+        return secondNum;
+    }
 };
+
+//npm run test fibonacci.spec.js
 
 // Do not edit below this line
 module.exports = fibonacci;
