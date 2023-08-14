a49f283dd2858f3217f87be1af862f92adea31dd
Donna <gretayeungg@gmail.com>
2023-08-14 12:39:44 +0800

    getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map(item => item.title);
 };
 
+//npm run test getTheTitles.spec.js
 // Do not edit below this line
 module.exports = getTheTitles;
